e22c1e0a0a8b51a162a2e78d5d340218e3bdbaf5
Alejandro Valle Torres <alejandrovaltor.x2@gmail.com>
2022-10-05 16:20:51 +0200

    Added create and update

--- HotelBooking.UnitTests/BookingControllerTests.cs
+++ HotelBooking.UnitTests/BookingControllerTests.cs
@@ -25,8 +25,8 @@ namespace HotelBooking.UnitTests
             DateTime endDate = new DateTime(2023, 10, 3);
             bookings = new List<Booking>
             {
-                new Booking { Id=1, StartDate=startDate, EndDate=endDate, CustomerId=1, RoomId=1},
-                new Booking { Id=2, StartDate=endDate, EndDate=endDate, CustomerId=2, RoomId=2},
+                new Booking { Id=1, StartDate=startDate, EndDate=endDate, CustomerId=1, RoomId=1, IsActive=false},
+                new Booking { Id=2, StartDate=startDate, EndDate=endDate, CustomerId=2, RoomId=2, IsActive=false},
             };
 
             var rooms = new List<Room>
@@ -38,7 +38,8 @@ namespace HotelBooking.UnitTests
             var customers = new List<Customer>
             {
                 new Customer { Id=1, Name="A", Email="A@A.com" },
-                new Customer { Id=2, Name="B", Email="B@B.com" }
+                new Customer { Id=2, Name="B", Email="B@B.com" },
+                new Customer { Id=3, Name="C", Email="C@C.com" }
             };
 
             // Create fake BookingRepository. 
@@ -116,6 +117,44 @@ namespace HotelBooking.UnitTests
             var booking = result.Value as Booking;
             // Assert
             Assert.Equal(idExpected, booking.Id);
+        }
+
+        [Theory]
+        [InlineData(3, 1)]
+        [InlineData(4, 2)]
+        public void CreateBooking_CreateIsCalled(int id, int idRoom)
+        {
+            // Act
+            DateTime startDate = new DateTime(2024, 10, 1);
+            DateTime endDate = new DateTime(2024, 10, 3);
+            Booking booking = new Booking { 
+                Id = id, 
+                StartDate = startDate, 
+                EndDate = endDate, 
+                CustomerId = 2, 
+                RoomId = idRoom
+            };
+            controller.Post(booking);
+
+            // Assert
+            fakeBookingRepository.Verify(x => x.Add(booking), Times.Once);
+        }
+
+        [Theory]
+        [InlineData(1, 3, true)]
+        [InlineData(2, 1, true)]
+        public void UpdateBooking_UpdateIsCalled(int id, int customerId, bool isActive)
+        {
+            // Act
+            fakeBookingRepository.Setup(x => x.Get(id)).Returns(bookings[id - 1]);
+            var result = controller.Get(id) as ObjectResult;
+            var booking = result.Value as Booking;
+            booking.CustomerId = customerId;
+            booking.IsActive = isActive;
+            controller.Put(id,booking);
+
+            // Assert
+            fakeBookingRepository.Verify(x => x.Edit(booking));
         }
 
         [Theory]
