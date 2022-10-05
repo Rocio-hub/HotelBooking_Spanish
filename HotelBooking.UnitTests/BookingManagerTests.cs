1637caf066be03c25719d38e67445d98f2517f71
Alejandro Valle Torres <alejandrovaltor.x2@gmail.com>
2022-10-05 11:16:10 +0200

    Added Test for create booking

--- HotelBooking.UnitTests/BookingManagerTests.cs
+++ HotelBooking.UnitTests/BookingManagerTests.cs
@@ -41,5 +41,61 @@ namespace HotelBooking.UnitTests
             Assert.NotEqual(-1, roomId);
         }
 
+        [Fact]
+        public void CreateBooking_ResultNotFalse()
+        {
+            // Arrange
+            Booking booking = new Booking();
+            DateTime startDate = new DateTime(2023, 10, 1);
+            DateTime endDate = new DateTime(2023, 10, 3);
+
+            booking.StartDate = startDate;
+            booking.EndDate = endDate;
+            booking.CustomerId = 1;
+            booking.RoomId = 1;
+
+            // Act
+            bool isCreate = bookingManager.CreateBooking(booking);
+            // Assert
+            Assert.True(isCreate);
+        }
+
+        [Fact]
+        public void CreateBooking_PastDate_ResultException()
+        {
+            // Arrange
+            Booking booking = new Booking();
+            DateTime startDate = new DateTime(2021, 10, 1);
+            DateTime endDate = new DateTime(2021, 10, 3);
+
+            booking.StartDate = startDate;
+            booking.EndDate = endDate;
+            booking.CustomerId = 1;
+            booking.RoomId = 1;
+
+            // Act
+            Action act = () => bookingManager.CreateBooking(booking);
+            // Assert
+            Assert.Throws<ArgumentException>(act);
+        }
+
+        [Fact]
+        public void CreateBooking_3Oct1Oct_ResultException()
+        {
+            // Arrange
+            Booking booking = new Booking();
+            DateTime startDate = new DateTime(2021, 10, 1);
+            DateTime endDate = new DateTime(2021, 10, 3);
+
+            booking.StartDate = endDate;
+            booking.EndDate = startDate;
+            booking.CustomerId = 1;
+            booking.RoomId = 1;
+
+            // Act
+            Action act = () => bookingManager.CreateBooking(booking);
+            // Assert
+            Assert.Throws<ArgumentException>(act);
+        }
     }
 }
